533c1ad3b72e2159bf2581eba597e98137ceb736
belaxie <1981253197@qq.com>
2022-05-07 00:39:53 +0800

    update

---
+++ precache-manifest.28e71903db19f4d9746111dd8ad49d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223f6bf3ebca58301251ee843a25a77e",
+    "url": "/tally/index.html"
+  },
+  {
+    "revision": "6dc134659aafb565d3ce",
+    "url": "/tally/js/app.82957b10.js"
+  },
+  {
+    "revision": "10e73bf9ad279f4df8a9",
+    "url": "/tally/js/chunk-vendors.83003d28.js"
+  },
+  {
+    "revision": "11ef934ab77a2f9b18870eb609b61517",
+    "url": "/tally/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tally/robots.txt"
+  }
+]);
